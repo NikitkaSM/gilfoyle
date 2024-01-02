4001e28b09782102c9e6da621899ee9ff0e17ba5
Nikita Smolin <nikitkasmo@gmail.com>
2024-01-02 15:27:07 +0300

    added description to /configre

--- src/cogs/slash_commands/configure.py
+++ src/cogs/slash_commands/configure.py
@@ -1,12 +1,14 @@
 from disnake import ApplicationCommandInteraction, TextChannel
 from disnake.ext import commands
 
+DESCRIPTION = 'Укажите канал куда будут пересылаться сообщения'
+
 
 class ConfigureSlashCommand(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.slash_command()
+    @commands.slash_command(description=DESCRIPTION)
     @commands.has_permissions(administrator=True)
     async def configure(self, inter: ApplicationCommandInteraction, channel: TextChannel):
         await inter.response.send_message(channel.name)
